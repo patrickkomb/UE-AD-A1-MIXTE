ca0e6792f3d3cd3214bd94d568738c786c86d3c0
patrickkomb <mail@example.com>
2025-10-01 16:47:43 +0200

    fix issue update rating movie service

--- movie/resolvers.py
+++ movie/resolvers.py
@@ -15,7 +15,6 @@ def actor_with_id(_,info,_id):
                 return actor
 
 def update_movie_rate(_,info,_id,_rate):
-    newmovies = {}
     newmovie = {}
     with open('{}/data/movies.json'.format("."), "r") as rfile:
         movies = json.load(rfile)
@@ -23,15 +22,11 @@ def update_movie_rate(_,info,_id,_rate):
             if movie['id'] == _id:
                 movie['rating'] = _rate
                 newmovie = movie
-                newmovies = movies
     with open('{}/data/movies.json'.format("."), "w") as wfile:
-        json.dump(newmovies, wfile)
+        json.dump(movies, wfile)
     return newmovie
 
 '''
-Les variables newmovies et newmovie sont inutiles ?
-Tester cette version du code :
-
 def update_movie_rate(_,info,_id,_rate):
     #with open('./data/movies.json', "r") as rfile:
     with open('{}/data/movies.json'.format("."), "r") as rfile:
