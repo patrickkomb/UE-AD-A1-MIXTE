a8606784ca41b20fdf3d3013aab879e18bd03ac2
Noé JOUAN <jouan.noe@gmail.com>
2025-11-20 11:52:20 +0100

    Add REST user.py

--- user/user.py
+++ user/user.py
@@ -1,27 +1,70 @@
-# REST API
-from flask import Flask, render_template, request, jsonify, make_response
-import requests
+from flask import Flask, request, jsonify, make_response
 import json
-from werkzeug.exceptions import NotFound
-
-# CALLING gRPC requests
-import grpc
-from concurrent import futures
-import booking_pb2
-import booking_pb2_grpc
-import movie_pb2
-import movie_pb2_grpc
-
-# CALLING GraphQL requests
-# todo to complete
 
 app = Flask(__name__)
 
-PORT = 3004
+PORT = 3203
 HOST = '0.0.0.0'
 
 with open('{}/data/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
+
+def write(users):
+    with open('{}/data/users.json'.format("."), 'w') as f:
+        full = {'users': users}
+        json.dump(full, f)
+
+@app.route("/", methods=['GET'])
+def home():
+   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
+@app.route("/users/<userid>", methods=['GET'])
+def get_user_byid(userid):
+    for user in users:
+        if str(user["id"]) == str(userid):
+            res = make_response(jsonify(user),200)
+            return res
+    return make_response(jsonify({"error":"User ID not found"}),404)
+
+@app.route("/users", methods=['POST'])
+def add_user():
+    req = request.get_json()
+
+    for user in users:
+        if str(user["id"]) == str(req["id"]):
+            print(user["id"])
+            print(req["id"])
+            return make_response(jsonify({"error":"User ID already exists"}),409)
+
+    users.append(req)
+    write(users)
+    res = make_response(jsonify({"message":"user added"}),200)
+    return res
+
+@app.route("/users/<userid>", methods=['PUT'])
+def update_user(userid):
+    req = request.get_json()
+
+    for user in users:
+        if str(user["id"]) == str(userid):
+            user.update(req)
+            res = make_response(jsonify(user),200)
+            write(users)
+            return res
+
+    res = make_response(jsonify({"error":"user ID not found"}),404)
+    return res
+
+@app.route("/users/<userid>", methods=['DELETE'])
+def del_user(userid):
+    for user in users:
+        if str(user["id"]) == str(userid):
+            users.remove(user)
+            write(users)
+            return make_response(jsonify(user),200)
+
+    res = make_response(jsonify({"error":"user ID not found"}),404)
+    return res
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
