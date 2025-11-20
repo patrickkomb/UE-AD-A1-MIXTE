c112217a19edd6fdb89453c6fdb2b7d38f5eba90
Noé JOUAN <jouan.noe@gmail.com>
2025-11-20 15:50:39 +0100

    Added repository and env to handle MongoDB/Local json data operations (Movie)

---
+++ movie/repository.py
@@ -0,0 +1,62 @@
+import json
+from pymongo import MongoClient
+from env import USE_MONGO, MONGO_URL
+
+MOVIES_FILE = "{}/data/movies.json"
+ACTORS_FILE = "{}/data/actors.json"
+
+def get_movie_repository():
+    if USE_MONGO:
+        return MongoMovieRepository()
+    return JsonMovieRepository()
+
+def get_actor_repository():
+    if USE_MONGO:
+        return MongoActorRepository()
+    return JsonActorRepository()
+
+class JsonMovieRepository:
+    def load(self):
+        with open(MOVIES_FILE.format("."), "r") as file:
+            return json.load(file)["movies"]
+
+    def save(self, movies):
+        with open(MOVIES_FILE.format("."), "w") as file:
+            json.dump({"movies": movies}, file)
+
+class MongoMovieRepository:
+    def __init__(self):
+        self.client = MongoClient(MONGO_URL)
+        self.db = self.client.get_database()
+        self.collection = self.db["movies"]
+
+    def load(self):
+        return list(self.collection.find({}))
+
+    def save(self, movies):
+        self.collection.delete_many({})
+        if movies:
+            self.collection.insert_many(movies)
+
+class JsonActorRepository:
+    def load(self):
+        with open(ACTORS_FILE.format("."), "r") as file:
+            return json.load(file)["actors"]
+
+    def save(self, actors):
+        with open(ACTORS_FILE.format("."), "w") as file:
+            json.dump({"actors": actors}, file)
+
+class MongoActorRepository:
+    def __init__(self):
+        self.client = MongoClient(MONGO_URL)
+        self.db = self.client.get_database()
+        self.collection = self.db["actors"]
+
+    def load(self):
+        return list(self.collection.find({}))
+
+    def save(self, actors):
+        self.collection.delete_many({})
+        if actors:
+            self.collection.insert_many(actors)
--- movie/resolvers.py
+++ movie/resolvers.py
@@ -1,74 +1,55 @@
-import json
+from repository import get_movie_repository, get_actor_repository
 
-MOVIE_FILE = "{}/data/movies.json"
-ACTOR_FILE = "{}/data/actors.json"
-
+movie_repo = get_movie_repository()
+actor_repo = get_actor_repository()
 
 # TODO: Ajouter vérification user est admin pour les routes admin
-
-# -- UTILS --
-def load_movies():
-    with open(MOVIE_FILE.format("."), "r") as file:
-        return json.load(file)["movies"]
-
-def save_movies(movies):
-    with open(MOVIE_FILE.format("."), "w") as file:
-        json.dump({"movies": movies}, file)
-
-def load_actors():
-    with open(ACTOR_FILE.format("."), "r") as file:
-        return json.load(file)["actors"]
-
-def save_actors(actors):
-    with open(ACTOR_FILE.format("."), "w") as file:
-        json.dump({"actors": actors}, file)
-
 # -- QUERIES --
 
 # Movie
 def movie_with_id(_, info, _id):
-    for movie in load_movies():
+    for movie in movie_repo.load():
         if movie["id"] == _id:
             return movie
     return None
 
 def all_movies(_, info):
-    return load_movies()
+    return movie_repo.load()
 
 def movie_with_title(_, info, _title):
-    for movie in load_movies():
+    for movie in movie_repo.load():
         if movie["title"].lower() == _title.lower():
             return movie
     return None
 
 def resolve_actors_in_movie(movie, info):
-    actors = load_actors()
+    actors = actor_repo.load()
     return [actor for actor in actors if movie["id"] in actor["films"]]
 
 # Actor
 def actor_with_id(_, info, _id):
-    for actor in load_actors():
+    for actor in actor_repo.load():
         if actor["id"] == _id:
             return actor
     return None
 
 def all_actors(_, info):
-    return load_actors()
+    return actor_repo.load()
 
 # -- MUTATIONS -- #
 
 # Movie
 def update_movie_rate(_, info, _id, _rate):
-    movies = load_movies()
+    movies = movie_repo.load()
     for movie in movies:
         if movie["id"] == _id:
             movie["rating"] = _rate
-            save_movies(movies)
+            movie_repo.save(movies)
             return movie
     return None
 
 def add_movie(_, info, _id, _title, _director, _rating):
-    movies = load_movies()
+    movies = movie_repo.load()
 
     for movie in movies:
         if str(movie["id"]) == str(_id):
@@ -82,22 +63,22 @@ def add_movie(_, info, _id, _title, _director, _rating):
     }
 
     movies.append(new_movie)
-    save_movies(movies)
+    movie_repo.save(movies)
     return new_movie
 
 def delete_movie(_, info, _id):
-    movies = load_movies()
+    movies = movie_repo.load()
 
     for movie in movies:
         if str(movie["id"]) == str(_id):
             movies.remove(movie)
-            save_movies(movies)
+            movie_repo.save(movies)
             return movie
     return None
 
 # Actor
 def add_actor(_, info, _id, _firstname, _lastname, _birthyear):
-    actors = load_actors()
+    actors = actor_repo.load()
 
     for actor in actors:
         if str(actor["id"]) == str(_id):
@@ -112,11 +93,11 @@ def add_actor(_, info, _id, _firstname, _lastname, _birthyear):
     }
 
     actors.append(new_actor)
-    save_actors(actors)
+    actor_repo.save(actors)
     return new_actor
 
 def add_movie_to_actor(_, info, _movie_id, _actor_id):
-    actors = load_actors()
+    actors = actor_repo.load()
     movie = movie_with_id(_, info, _movie_id)
 
     if movie is None:
@@ -126,16 +107,16 @@ def add_movie_to_actor(_, info, _movie_id, _actor_id):
         if str(actor["id"]) == str(_actor_id):
             if _movie_id not in actor["films"]:
                 actor["films"].append(_movie_id)
-                save_actors(actors)
+                actor_repo.save(actors)
             return actor
     return None
 
 def delete_actor(_, info, _id):
-    actors = load_actors()
+    actors = actor_repo.load()
 
     for actor in actors:
         if str(actor["id"]) == str(_id):
             actors.remove(actor)
-            save_actors(actors)
+            actor_repo.save(actors)
             return actor
     return None
